9fb026ee3022bc4ac495d3a5250ca3700b33bc80
kalinin-ai <alexey.kalinin@exponenta.ru>
2022-11-24 16:28:06 +0300

    add parse_options template, delete cfg

---
+++ utils/parse_conig.py
@@ -0,0 +1,12 @@
+def parse_config(path: str, ) -> dict:
+    """
+    Parse yaml file
+
+    Args:
+        path: str, path to config
+
+    Returns:
+        cfg: dict, dictionary with new params
+    """
+
+    return cfg
